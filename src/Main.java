a1a3c4421a0b7bbc8550a912d09517d69438c54e
rafaelsilvafirmo <rafasfirmo@gmail.com>
2026-09-06 19:03:37 -0300

    Create Bootcamp, Conteudo and Dev classes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
-import java.time.LocalDate;
+import br.com.dio.desafio.dominio.Bootcamp;
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
+import br.com.dio.desafio.dominio.Mentoria;
 
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
@@ -18,8 +22,37 @@ public class Main {
         mentoria.setDescricao("descrição mentoria java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java");
+        bootcamp.setDescricao("Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devFulano = new Dev();
+        devFulano.setNome("Fulano de Tal");
+        devFulano.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos Fulano: " + devFulano.getConteudosInscritos());
+        devFulano.progredir();
+        devFulano.progredir();
+        System.out.println("=====");
+        System.out.println("Conteúdos inscrito Fulano: " + devFulano.getConteudosInscritos());
+        System.out.println("Conteúdos concluidos Fulano: " + devFulano.getConteudosConcluidos());
+        System.out.println("XP: " + devFulano.calcularTotalXp());
+
+        System.out.println("------------------------------");
+
+        Dev devCiclano = new Dev();
+        devCiclano.setNome("Ciclano de Tal");
+        devCiclano.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos Ciclano: " + devCiclano.getConteudosInscritos());
+        devCiclano.progredir();
+        devCiclano.progredir();
+        devCiclano.progredir();
+        System.out.println("=====");
+        System.out.println("Conteúdos inscritos Ciclano: " + devCiclano.getConteudosInscritos());
+        System.out.println("Conteúdos concluidos Ciclano: " + devCiclano.getConteudosConcluidos());
+        System.out.println("XP : " + devCiclano.calcularTotalXp());
+
     }
 }
